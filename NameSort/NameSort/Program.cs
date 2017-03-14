34337dda0efeed1d3d309561f2e65ef9258307a4
Thomas Clarke <t.b.clarke@cqu.edu.au>
2017-03-14 22:26:13 +1000

    Commit initial release

---
+++ NameSort/NameSort/Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using Microsoft.VisualBasic.FileIO;
+namespace NameSort
+{
+    public class Program
+    {
+        //write the final sorted List to a file
+        public static String[] WriteList(List<String[]> fileContent, String arg)
+        {
+            String[] r = new String[3];
+            try
+                {
+                    //Append -sorted to the file path before the extension name before saving - files without an extension are not supported
+                    string filePath = @"" + arg.Substring(0,arg.LastIndexOf(".")) + "-sorted" + arg.Substring(arg.LastIndexOf("."));  
+                    String result = "";
+                    foreach(String[] f in fileContent)
+                    {
+                        result += f.GetValue(0) + ", " + f.GetValue(1) + '\r' + '\n';
+                    }
+
+                    //remove the last line break
+                    result = result.Substring(0, result.Length - 2);
+                    File.WriteAllText(filePath, result);
+                    r.SetValue("Success",0);
+                    r.SetValue(filePath, 1);
+                    r.SetValue(result, 2);
+                }
+            catch
+            {
+                r.SetValue("FailWrite",0);
+            }
+            return r;
+        }
+
+        //sort the contents of the List object by last name then by first name
+        public static List<String[]> SortList(List<String[]> fileContent)
+        {
+            //Sort by first name initially
+            fileContent.Sort((a1, a2) => a1.GetValue(1).ToString().CompareTo(a2.GetValue(1).ToString()));
+            //Then by last name
+            fileContent.Sort((a1, a2) => a1.GetValue(0).ToString().CompareTo(a2.GetValue(0).ToString()));
+            return fileContent;
+        }
+
+        //read the entered file name's contents into a variable
+        public static List<String[]> ReadFile(string arg)
+        {
+            try
+            {
+                String[] r = new String[2];
+                List<String[]> fileContent = new List<string[]>();
+                using (FileStream reader =
+                            File.OpenRead(@"" + arg))
+                using (TextFieldParser parser = new TextFieldParser(reader))
+                {
+                    // we want to trim the white space in case there is a peculiarity in the entered format
+                    parser.TrimWhiteSpace = true;
+                    parser.Delimiters = new[] { "," };
+                    //Supporting fields enclosed in quotes in case this is required
+                    parser.HasFieldsEnclosedInQuotes = true;
+                    while (!parser.EndOfData)
+                    {
+                        string[] line = parser.ReadFields();
+                        fileContent.Add(line);
+                    }
+                }
+                return fileContent;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        //primary handler function to call other functions
+        public static string[] RunSort(string arg)
+        {
+            String[] r = new String[3];
+            List<String[]> fileContent = new List<string[]>();
+            fileContent = ReadFile(arg);
+            if(fileContent == null)
+            {
+                r.SetValue("FailRead",0);
+            }
+            else
+            {
+                fileContent = SortList(fileContent);
+                String[] result = WriteList(fileContent, arg);
+                return result;
+            }
+            return r;
+         }
+
+        //Main function to take command line arguments and write back messages to the user
+        static void Main(string[] args)
+        {
+            //User needs to supply one argument and one argument only
+            if (args.Length != 1)
+            {
+                System.Console.WriteLine("Please provide one parameter, representing the file name, to the application, e.g. C:\\sort.txt - you have provided {0} parameters.", args.Length);
+            }
+            else
+            {
+                String arg = args.GetValue(0).ToString();
+                String[] result  = RunSort(arg);
+                if(result.GetValue(0).ToString() == "Success")
+                {
+                    System.Console.WriteLine("File successfully sorted and saved to " + result.GetValue(1));
+                }
+                else if(result.GetValue(0).ToString() == "FailRead")
+                {
+                    System.Console.WriteLine("There was an error opening the provided file - please ensure you have provided a valid file path, the application has access to it and the file is comma-separated in the format FirstName, LastName");
+                }
+                else if(result.GetValue(0).ToString() == "FailWrite")
+                {
+                    System.Console.WriteLine("An error occurred writing the final sorted file - please ensure the application has permission to write to the directory it read the file from.");
+                }
+            }
+            System.Console.WriteLine("Press any key to exit...");
+            System.Console.ReadKey();
+        }
+    }
+}
---
+++ NameSortTests/NameSortTests/UnitTest1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using NameSort;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace NameSortTests
+{
+    [TestClass]
+    public class UnitTests
+    {
+
+        //This tests one list against the sort method with different permutations of first/last difference
+        [TestMethod]
+        public void SortListTest()
+        {
+            //setup dummy data for testing
+            List<String[]> input = new List<String[]>();
+            List<String[]> expectedOutput = new List<String[]>();
+            String[] row1 = new String[] { "SMITH", "AARON" };
+            input.Add(row1);
+            String[] row2 = new String[] { "ANDERSON", "THOMAS" };
+            input.Add(row2);
+            String[] row3 = new String[] { "JAMIESON", "JAMES" };
+            input.Add(row3);
+            String[] row4 = new String[] { "HASTINGS", "STEVE" };
+            input.Add(row4);
+            String[] row5 = new String[] { "SMITH", "JOHN" };
+            input.Add(row5);
+            String[] row6 = new String[] { "KENT", "JOHN" };
+            input.Add(row6);
+            String[] row7 = new String[] { "SMITH", "AVERY" };
+            input.Add(row7);
+            expectedOutput.Add(row2);
+            expectedOutput.Add(row4);
+            expectedOutput.Add(row3);
+            expectedOutput.Add(row6);
+            expectedOutput.Add(row1);
+            expectedOutput.Add(row7);
+            expectedOutput.Add(row5);
+            Assert.AreEqual(true, expectedOutput.SequenceEqual(NameSort.Program.SortList(input)));
+        }
+
+        [TestMethod]
+        public void WriteListTest()
+        {
+            //setup dummy data for testing
+            List<String[]> input = new List<String[]>();
+            String[] row1 = new String[] { "SMITH", "AARON" };
+            String[] row2 = new String[] { "ANDERSON", "THOMAS" };
+            String[] row3 = new String[] { "JAMIESON", "JAMES" };
+            String[] row4 = new String[] { "HASTINGS", "STEVE" };
+            String[] row5 = new String[] { "SMITH", "JOHN" };
+            String[] row6 = new String[] { "KENT", "JOHN" };
+            String[] row7 = new String[] { "SMITH", "AVERY" };
+            input.Add(row2);
+            input.Add(row4);
+            input.Add(row3);
+            input.Add(row6);
+            input.Add(row1);
+            input.Add(row7);
+            input.Add(row5);
+            String expectedString = "ANDERSON, THOMAS" + '\r' + '\n' +
+                                        "HASTINGS, STEVE" + '\r' + '\n' +
+                                        "JAMIESON, JAMES" + '\r' + '\n' +
+                                        "KENT, JOHN" + '\r' + '\n' +
+                                        "SMITH, AARON" + '\r' + '\n' +
+                                        "SMITH, AVERY" + '\r' + '\n' +
+                                        "SMITH, JOHN";
+            String[] expectedOutput = new String[] {"Success","D:\\unittest-sorted.txt",expectedString};
+            Assert.AreEqual(true, expectedOutput.SequenceEqual(NameSort.Program.WriteList(input, "D:\\unittest.txt")));
+            String fileText;
+            try
+            {
+                using (var streamReader = new StreamReader(@"D:\\unittest-sorted.txt", Encoding.UTF8))
+                {
+                    fileText = streamReader.ReadToEnd();
+                    if (!fileText.Equals(expectedString))
+                    {
+                        Assert.Fail("File contents not correct");
+                    }
+                }
+            }
+            catch
+            {
+                Assert.Fail("File read failed. Could mean file does not exist.");
+            }
+        }
+
+        //Test the read file method
+        [TestMethod]
+        public void ReadFileTest()
+        {
+            //write a dummy file for testing
+            string filePath = @"D:\\fileread-unittest.txt";
+            String fileContents = "ANDERSON, THOMAS" + '\r' + '\n' +
+                                        "HASTINGS, STEVE" + '\r' + '\n' +
+                                        "JAMIESON, JAMES" + '\r' + '\n' +
+                                        "KENT, JOHN" + '\r' + '\n' +
+                                        "SMITH, AARON" + '\r' + '\n' +
+                                        "SMITH, AVERY" + '\r' + '\n' +
+                                        "SMITH, JOHN";
+            List<String[]> expectedOutput = new List<String[]>();
+            String[] row1 = new String[] { "SMITH", "AARON" };
+            String[] row2 = new String[] { "ANDERSON", "THOMAS" };
+            String[] row3 = new String[] { "JAMIESON", "JAMES" };
+            String[] row4 = new String[] { "HASTINGS", "STEVE" };
+            String[] row5 = new String[] { "SMITH", "JOHN" };
+            String[] row6 = new String[] { "KENT", "JOHN" };
+            String[] row7 = new String[] { "SMITH", "AVERY" };
+            expectedOutput.Add(row2);
+            expectedOutput.Add(row4);
+            expectedOutput.Add(row3);
+            expectedOutput.Add(row6);
+            expectedOutput.Add(row1);
+            expectedOutput.Add(row7);
+            expectedOutput.Add(row5);
+            File.WriteAllText(filePath, fileContents);
+            List<String[]> result = NameSort.Program.ReadFile(filePath);
+
+            //because the arrays reference different objects, we need to perform a big loop through them all
+            if (result.Count == expectedOutput.Count)
+            {
+                for (int i = 0; i < result.Count; i++)
+                {
+                    if (result[i].Length.Equals(expectedOutput[i].Length))
+                    {
+                        for (int j = 0; j < result[i].Length; j++)
+                            if (result[i][j].Equals(expectedOutput[i][j]))
+                            {
+                            }
+                            else
+                            {
+                                Assert.Fail("String " + i + "," + j + "did not match between arrays");
+                            }
+                    }
+                    else
+                    {
+                        Assert.Fail("File arrays are different lengths.");
+                    }
+                }
+            }
+            else
+            {
+                Assert.Fail("File lists are different lengths.");
+            }
+        }
+    }
+}
